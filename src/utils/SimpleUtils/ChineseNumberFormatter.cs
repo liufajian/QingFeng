4cd2f62f1e13d938c8121c2bdd6e4c2f339de53e
liufajian <liufajian@msn.com>
2023-08-25 19:26:22 +0800

    ChineseNumberFormatter

---
+++ src/utils/SimpleUtils/ChineseNumberFormatter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace QingFeng.SimpleUtils
+{
+    internal class ChineseNumberFormatter
+    {
+        
+    }
+}
